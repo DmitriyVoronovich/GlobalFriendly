e5a12b4e03b0b9063381fc24cb613c8867a6c94d
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-10-12 13:40:17 +0300

    completed lesson 16

--- src/components/profile/posts/post/Post.tsx
+++ src/components/profile/posts/post/Post.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-const Post = () => {
+type PostPropsType = {
+    message: string
+    like: number
+}
+
+const Post:React.FC<PostPropsType> = (props:PostPropsType) => {
     return (
         <div className={s.item}>
             <img src={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'}/>
-            post
+            {props.message}
             <div>
-                <span>like</span>
+                <span>{props.like} </span><span>like</span>
             </div>
         </div>
     );
