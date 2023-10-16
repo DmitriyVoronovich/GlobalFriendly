a242fd742e88758998f93ac3a76d462b23bbafd0
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-10-16 11:57:24 +0300

    completed lessons 20-21

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <section className={'dialogs'}>
-            Dialogs
+        <section className={s.dialogs}>
+            <h2>Dialogs</h2>
+            <div className={s.dialogWrapper}>
+                <div className={s.dialogsItems}>
+                    <div className={s.item + ' ' + s.active}>
+                        <NavLink to={''}>Dmitriy</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to={''}>Oleg</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to={''}>Kate</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to={''}>Oly</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to={''}>Igor</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to={''}>Pavel</NavLink>
+                    </div>
+                </div>
+                <div className={s.dialogMassages}>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                    <div className={s.massagesItem}>
+                        Hi, how are you?
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
