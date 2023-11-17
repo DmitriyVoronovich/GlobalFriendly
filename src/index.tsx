361cbda9a9626f5f4bffa90c5c6de5203032d13d
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-11-17 12:47:39 +0300

    completed lesson 37

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addMessage, addPost, state} from "./redux/state";
+import {store} from "./redux/state";
 
 ReactDOM.render(
-    <App state={state} addPost={addPost} addMessage={addMessage}/>,
+    <App state={store.getState()}
+         addPost={store.addPost.bind(store)}
+         addMessage={store.addMessage.bind(store)}/>,
   document.getElementById('root')
 );
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,41 +1,44 @@
-export let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', like: 15},
-            {id: 1, message: 'It\'s my first post!', like: 20}
-        ]
-    },
-    dialogsPage: {
-        dialog: [
+export let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', like: 15},
+                {id: 1, message: 'It\'s my first post!', like: 20}
+            ]
+        },
+        dialogsPage: {
+            dialog: [
+                {id: 1, name: 'Dmitriy'},
+                {id: 2, name: 'Oleg'},
+                {id: 3, name: 'Kate'},
+                {id: 4, name: 'Oly'},
+                {id: 5, name: 'Igor'},
+                {id: 6, name: 'Pavel'}
+            ],
+            message: [
+                {id: 1, message: 'Hi, how are you?', you: true},
+                {id: 2, message: 'Yo', you: false},
+                {id: 3, message: 'GG', you: true},
+                {id: 4, message: 'Oh, no', you: true},
+                {id: 5, message: 'loser', you: false},
+                {id: 6, message: 'fuck you', you: false}
+            ]
+        },
+        sidebar: [
             {id: 1, name: 'Dmitriy'},
             {id: 2, name: 'Oleg'},
-            {id: 3, name: 'Kate'},
-            {id: 4, name: 'Oly'},
-            {id: 5, name: 'Igor'},
-            {id: 6, name: 'Pavel'}
-        ],
-        message: [
-            {id: 1, message: ' Hi, how are you?', you: true},
-            {id: 2, message: 'Yo', you: false},
-            {id: 3, message: 'GG', you: true},
-            {id: 4, message: 'Oh, no', you: true},
-            {id: 5, message: 'loser', you: false},
-            {id: 6, message: 'fuck you', you: false}
+            {id: 3, name: 'Kate'}
         ]
     },
-    sidebar: [
-        {id: 1, name: 'Dmitriy'},
-        {id: 2, name: 'Oleg'},
-        {id: 3, name: 'Kate'}
-    ]
-}
-
-export const addPost = (postMessage: string) => {
-    const post = {id: Math.random(),message: postMessage, like: 0 };
-    state.profilePage.posts.push(post);
-};
-
-export const addMessage = (newMessage: string) => {
-    const message = {id: Math.random(),message: newMessage, you: true};
-    state.dialogsPage.message.push(message)
+    getState() {
+        return this._state;
+    },
+    addPost(postMessage: string) {
+        const post = {id: Math.random(),message: postMessage, like: 0 };
+        this._state.profilePage.posts.push(post);
+    },
+    addMessage (newMessage: string) {
+        const message = {id: Math.random(),message: newMessage, you: true};
+        this._state.dialogsPage.message.push(message)
+    },
 }
