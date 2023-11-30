45607c9b1a9db53ed6c963fa8583d94c7d762d92
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2023-11-30 16:11:56 +0300

    code refactoring

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -23,23 +23,27 @@ const Dialogs: React.FC<DialogPropsType> = (props) => {
         setNewMessage('');
     };
 
+    const dialogs = state.dialog.map((item) => {
+        return (
+            <DialogItem key={item.id} id={item.id} name={item.name}/>
+        )
+    });
+
+    const message = state.message.map((item) => {
+        return (
+            <Message key={item.id} message={item.message} you={item.you}/>
+        )
+    });
+
     return (
         <section className={s.dialogs}>
             <h2>Dialogs</h2>
             <div className={s.dialogWrapper}>
                 <div className={s.dialogsItems}>
-                    {state.dialog.map((item) => {
-                        return (
-                            <DialogItem key={item.id} id={item.id} name={item.name}/>
-                        )
-                    })}
+                    {dialogs}
                 </div>
                 <div className={s.dialogMassages}>
-                    {state.message.map((item) => {
-                        return (
-                            <Message key={item.id} message={item.message} you={item.you}/>
-                        )
-                    })}
+                    {message}
                 </div>
             </div>
             <div className={s.dialogInput}>
